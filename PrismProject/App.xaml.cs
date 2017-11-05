6fb3c5dc0a9b9bda4f69c1287120afc3aa969477
droyiii <droyiii@gmail.com>
2017-11-05 11:02:56 -0600

    initiali check in

---
+++ PrismProject/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using PrismProject.Views;
+using Prism.Unity;
+using Xamarin.Forms;
+
+namespace PrismProject
+{
+  public partial class App : PrismApplication
+  {
+    public App()
+    {
+      InitializeComponent();
+
+
+    }
+
+    protected override void OnInitialized()
+    {
+      NavigationService.NavigateAsync("Navigation/MainPage").ContinueWith(t =>
+      {
+        if (t.Exception != null)
+          throw t.Exception;
+      });
+    }
+
+    protected override void RegisterTypes()
+    {
+      Container.RegisterTypeForNavigation<NavigationPage>("Navigation");
+      Container.RegisterTypeForNavigation<MainPage>("MainPage");
+    }
+
+
+    protected override void OnStart()
+    {
+      // Handle when your app starts
+    }
+
+    protected override void OnSleep()
+    {
+      // Handle when your app sleeps
+    }
+
+    protected override void OnResume()
+    {
+      // Handle when your app resumes
+    }
+  }
+}
---
+++ PrismProject/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PrismProject.ViewModels
+{
+  public class MainPageViewModel
+  {
+    public MainPageViewModel()
+    {
+    }
+  }
+}
